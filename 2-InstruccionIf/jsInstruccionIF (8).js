006f403ee30f541446052760f004c975d597cd82
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-02 16:57:36 -0300

    Realizacion del Ejercicio

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,13 @@
 function Mostrar()
 {
 //tomo la edad  
+var age;
+var Estadocivil = document.getElementById("estadoCivil").value;
+ age= document.getElementById("edad").value;
 
-	
-
+ if(age >=18 && Estadocivil == "Soltero")
+ {
+      alert("Es soltero y no es menor");
+ }
 
 }//FIN DE LA FUNCIÓN
