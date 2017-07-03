933205f60c4d95d8b8b302b0d21a2d6702705eb9
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-03 10:21:20 -0300

    Realización del Ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
+var Mes = document.getElementById('mes').value;
+switch (Mes)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Junio":
+    alert("Falta para el invierno.");
+    break;
+    case "Julio":
+    case "Agosto":
+    alert("Abrigate que hace frio.");
+    break;
+    default:
+    alert("Ya pasamos el frio, ahora calor!!!.");
+    break;
+}
 }//FIN DE LA FUNCIÓN
