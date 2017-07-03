3f70b3a7763b5d0a144a64c3daf64177db8e092e
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-03 09:06:35 -0300

    Corrección del ejercicio

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -5,7 +5,7 @@ var age;
 var Estadocivil = document.getElementById("estadoCivil").value;
  age= document.getElementById("edad").value;
 
- if(age >=18 && Estadocivil == "Soltero")
+ if (age >=18 && Estadocivil == "Soltero")
  {
       alert("Es soltero y no es menor");
  }
