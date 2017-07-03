28542867aa1703bd35d762e197385021813f6f90
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-03 09:37:12 -0300

    Corrección del Ejercicio

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,13 +3,13 @@ function Mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	var num;
 	num = Math.floor (Math.random () * (10 - 1)) + 1;
-	if (num == 10 || num == 9)
+	if (num >= 9)
 	{
 		alert("Excelente");
 	}
 	else 
 	{
-		if (num >= 4 && num <=8)
+		if (num >= 4)
 		{
 			alert("Aprobó");
 		}
