5a9dc4168f94347c664131ccf3649e3172b1d5f8
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-10 12:18:20 -0300

    Realización del ejercicio

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,10 +1,34 @@
 function Mostrar()
 {
-	var contador=0;
-	// declarar variables	
+	// declarar variables
+	var max;
+	var min;
+	var num;
 	var respuesta=true;
+	var primera =true;
 	while(respuesta!=false)
 	{
-				
+		num=prompt("Ingrese un número");
+		while(isNaN(num))
+		{
+			num=prompt("Ingrese un número");
+		}
+		if(primera==true)
+		{
+			max=num;
+			min=num;
+			primera=false;
+		}
+		else if(num<min)
+		{
+			min=num;
+		}
+		else if(num>max)
+		{
+			max=num;
+		}
+		respuesta=confirm("Desea continuar?");
 	}
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
 }//FIN DE LA FUNCIÓN
