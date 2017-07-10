c5a2216b4289987e6929a656160dbd9d93945db9
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-10 12:19:20 -0300

    Comenzando el ejercicio

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,10 +1,43 @@
 function Mostrar()
 {
-	var contador=0;
 	//declarar contadores y variables	
+	var num;
+	var contadorcero=0;
+	var contadorposi=0;
+	var contadornega=0;
+	var num;
+	var posi=0;
+	var nega=0;
 	var respuesta=true;
-	while(respuesta!=false)
+	var sumaposi=0;
+	var sumanega=0;
+	var promeposi;
+	var promenega;
+	var par;
+	while(respuesta==true)
 	{
-			
+		num=parseInt(prompt("Ingrese un número"));
+		while (isNaN(num))
+		{
+			num=parseInt(prompt("Ingrese un número"));
+		}
+		
+		if(num<0)
+		{
+			nega=num;
+			contadornegativos++;
+		}
+		else if(num>0)
+		{
+			posis=num;
+			contadorpositivos++;
+		}
+		else
+		{
+			contadorcero++;
+		}
+		
+		respuesta=confirm("Desea continuar?");
 	}
+	document.write()
 }//FIN DE LA FUNCIÓN
