d6d683706e4916bd8601f7eadc718c2d84405ce7
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-12 08:58:44 -0300

    Correccion del ejercicio

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,15 +2,15 @@ function Mostrar()
 {
 	//declarar contadores y variables	
 	var num;
+	var respuesta=true;
 	var contadorcero=0;
 	var contadorposi=0;
 	var contadornega=0;
-	var respuesta=true;
 	var sumaposi=0;
 	var sumanega=0;
 	var promeposi;
 	var promenega;
-	var contadorpar;
+	var contadorpar=0;
 	var diferenciaposinega;
 	while(respuesta==true)
 	{
@@ -42,14 +42,14 @@ function Mostrar()
 	}
 	promenega=sumanega/contadornega;
 	promeposi=sumaposi/contadorposi;
-	diferenciaposinega=sumaposi-sumanega;
-	document.write("La suma de los negativos es de: "+sumanega+"</br>");
-	document.write("La suma de los positivos es de: "+sumaposi+"</br>");
-	document.write("La cantidad de negativos es de: "+contadornega+"</br>");
-	document.write("La cantidad de positivos es de: "+contadorposi+"</br>");
-	document.write("La cantidad de ceros es de: "+contadorcero+"</br>");
-	document.write("La cantidad de números pares es de: "+contadorpar+"</br>");
-	document.write("El promedio de negativos es de: "+promenega+"</br>");
-	document.write("El promedio de positivos es de: "+promeposi+"</br>");
-	document.write("La diferencia entre positivos y negativos es de: "+diferenciaposinega+"</br>");
+	diferenciaposinega=sumaposi+sumanega;
+	document.write("La suma de los negativos es de: "+sumanega+"</br></br>");
+	document.write("La suma de los positivos es de: "+sumaposi+"</br></br>");
+	document.write("La cantidad de negativos es de: "+contadornega+"</br></br>");
+	document.write("La cantidad de positivos es de: "+contadorposi+"</br></br>");
+	document.write("La cantidad de ceros es de: "+contadorcero+"</br></br>");
+	document.write("La cantidad de números pares es de: "+contadorpar+"</br></br>");
+	document.write("El promedio de negativos es de: "+promenega+"</br></br>");
+	document.write("El promedio de positivos es de: "+promeposi+"</br></br>");
+	document.write("La diferencia entre positivos y negativos es de: "+diferenciaposinega+"</br></br>");
 }//FIN DE LA FUNCIÓN
