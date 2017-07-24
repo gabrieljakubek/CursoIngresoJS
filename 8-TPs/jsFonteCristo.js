e2e4ea568a4997b56fbb8404d8ad4af51b370a98
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-24 12:17:50 -0300

    Realizacion del ejercicio

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -9,5 +9,99 @@ e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos
 */
 function ComenzarIngreso () 
 {
- 	
+ 	var num;
+    var contpar=0;
+    var contimpar=0;
+    var contdivisibles=0;
+}
+function NumerosPares() 
+{
+    num = document.getElementById("numero").value;
+    if(num<1)
+    {
+        alert("Ingrese un número positivo");
+    }
+    else
+    {
+        for(i=1;i<num;i++)
+        {
+            if(i%2==0)
+            {
+                contpar++;
+            }
+        }
+    }
+    alert("La cantidad de úmeros pares es de: "+contpar);
+}
+function NumerosImpares() 
+{
+    num = document.getElementById("numero").value;
+    if(num<1)
+    {
+        alert("Ingrese un número positivo");
+    }
+    else
+    {
+        for(i=1;i<num;i++)
+        {
+            if(i%2!=0)
+            {
+                contimpar++;
+            }
+        }
+    }
+    alert("La cantidad de úmeros pares es de: "+contimpar);
+}
+function NumerosDivisibles() 
+{
+    num = document.getElementById("numero").value;
+    if(num<1)
+    {
+        alert("Ingrese un número positivo");
+    }
+    else
+    {
+        for(i=100;i<=100;i++)
+        {
+            if(num%i==0)
+            {
+                contdivisibles++;
+            }
+        }
+    }
+    alert("La cantidad de úmeros pares es de: "+contdivisibles);
+}
+function VerificarPrimo() 
+{
+    num = document.getElementById("numero").value;
+    if(num<1)
+    {
+        alert("Ingrese un número positivo");
+    }
+    else
+    {
+        for(i=2;i<num;i++)
+        {
+            if(num%i==0&&num!=2)
+            {
+                alert("El número no es primo");
+                break;
+            }
+            else
+            {
+                alert("El número es primo");
+            }
+        }
+    }
+}
+function NumerosPrimos() 
+{
+    num = document.getElementById("numero").value;
+    if(num<1)
+    {
+        alert("Ingrese un número positivo");
+    }
+    else
+    {
+    }
 }
