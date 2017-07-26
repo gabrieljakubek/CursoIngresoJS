090798976d141aba4ca76e5b84eb292ca1ec8316
gabrieljakubek <gabrieljakubek@gmail.com>
2017-07-26 12:16:30 -0300

    Realizacion del modelo de Parcial

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+	var numuno = parseInt(prompt("Ingrese un número"));
+	var numdos = parseInt(prompt("Ingrese un número"));
+	var resultado;
+	if (numuno==numdos)
+	{
+		resultado=numuno*numdos;
+	}
+	else if (numuno>numdos)
+	{
+		resultado=numuno-numdos;
+	}
+	else
+	{
+		resultado=numuno+numdos;
+	}
+	document.write("El resultado es: "+resultado);
 }
 
